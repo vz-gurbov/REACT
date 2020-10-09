5ec3436c6b5c31b3da1b1e719d93c39bfa0be416
vz-gurbov <vz.gurbov@abv.bg>
2020-10-09 12:19:24 +0300

    Updates

---
+++ precache-manifest.b79401dbc1381dd5b74627a754358835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774d2afd9c84ddbc7f2f9e9bef1c2c95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cec5aa9d4a0c9bf52ee",
+    "url": "/static/css/main.9c6ee4ce.chunk.css"
+  },
+  {
+    "revision": "f9e9f67bad663953fe5d",
+    "url": "/static/js/2.d39f5f4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d39f5f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cec5aa9d4a0c9bf52ee",
+    "url": "/static/js/main.b7f5893f.chunk.js"
+  },
+  {
+    "revision": "712a493cb6bbaa95b925",
+    "url": "/static/js/runtime-main.cef06927.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
